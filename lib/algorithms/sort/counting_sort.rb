7e30823330ff12ee3ff1764a0ab138174b7d9335
uchennafokoye <okoyefaithful@gmail.com>
2016-04-09 20:11:10 -0500

    Added Counting Sort

--- lib/algorithms/sort/counting_sort.rb
+++ lib/algorithms/sort/counting_sort.rb
@@ -1,5 +1,5 @@
 require 'sorting_interface'
-class QuickSort
+class CountingSort
   # Important Characteristics
   # 1. Worst case is O(n ^ 2) but typically runs O (n log n)
   # 2. Not stable sort so it might change the occurence of two similar elements
@@ -15,42 +15,45 @@ class QuickSort
   #    - Array
   # Example:
   #     array = [3, 2, 4, 5, 6, 1]
-  #     array = QuickSort.order(array)
+  #     array = CountingSort.order(array)
   #     puts array # Output:  [1, 2, 3, 4, 5, 6]
   # Example:
   #     array = [3, 2, 4, 5, 6, 1]
-  #     array = QuickSort.order(array, :desc => true)
+  #     array = CountingSort.order(array, :desc => true)
   #     puts array # Output: [6, 5, 4, 3, 2, 1]
   #
   # OR
   #
   # Example:
   #     array = [3, 2, 4, 5, 6, 1]
-  #     instance = QuickSort.new(array)
+  #     instance = CountingSort.new(array)
   #     puts instance.array # Output: [1, 2, 3, 4, 5, 6]
   #
   # Example:
   #     array = [3, 2, 4, 5, 6, 1]
-  #     instance = QuickSort.new(array, :desc => true)
+  #     instance = CountingSort.new(array, :desc => true)
   #     puts instance.array # Output: [6, 5, 4, 3, 2, 1]
   #
   #
   # ### Get Time Complexity:
   # Example:
-  #     best = QuickSort::TIME_COMPLEXITY_BEST
-  #     worst = QuickSort::TIME_COMPLEXITY_WORST
-  #     average = QuickSort::TIME_COMPLEXITY_AVERAGE
+  #     best = CountingSort::TIME_COMPLEXITY_BEST
+  #     worst = CountingSort::TIME_COMPLEXITY_WORST
+  #     average = CountingSort::TIME_COMPLEXITY_AVERAGE
 
   include SortingInterface
 
-  TIME_COMPLEXITY_WORST = "O(n^2)"
-  TIME_COMPLEXITY_AVERAGE = "O(n log n)"
-  TIME_COMPLEXITY_BEST = "O(n log n)"
-  SPACE_COMPLEXITY = "O(n log n)"
+  TIME_COMPLEXITY_WORST = "O(n)"
+  TIME_COMPLEXITY_AVERAGE = "O(n)"
+  TIME_COMPLEXITY_BEST = "O(n)"
+  SPACE_COMPLEXITY = "O(k+n)"
 
-  attr_reader :array, :desc
+  attr_reader :array, :desc, :k
 
-  def initialize array, desc=false
+  # For more on k variable, read on counting sort
+  # k indicates that every number in the container is in the range (0, k)
+  # where k is smaller than n, the container length
+  def initialize array, k, desc=false
     unless array.is_a? Enumerable
       raise "Please provide an array or other Enumerable"
     end
@@ -59,49 +62,46 @@ class QuickSort
     end
     @array = array
     @desc = desc
+    @k = k
     self.sort
   end
 
-  def self.order array, desc=false
-    instance = QuickSort.new array, desc=desc
+  def self.order array, k, desc=false
+    instance = CountingSort.new array, k, desc=desc
     instance.array
   end
 
   protected
   def sort
     n = array.length
-    quick_sort array, 0, n - 1
+    counting_sort array, k
   end
 
   def is_desc?
     @desc
   end
 
-  def comparison_symbol
-    is_desc? ? :>= : :<=
-  end
 
-  def quick_sort array, left, right
 
-    if left < right
-      pi = partition array, left, right
-      quick_sort array, left, pi - 1
-      quick_sort array, pi + 1, right
+  def counting_sort array, k
+    n = array.length
+    counting_array = Array.new k, 0
+    for i in 0...n
+      counting_array[array[i]] += 1
     end
 
-  end
-
-  def partition array, left, right
-    pivot = ((left + right) / 2).floor # could be randomly chosen but chose middle
-    swap array, pivot, right
-    store = left
-    for i in left..right-1
-      if array[i].send(comparison_symbol, array[right])
-        swap array, store, i
-        store = store + 1
+    idx = 0
+    comparison = is_desc? ? ((k-1).downto 0) : 0...k
+    for i in comparison
+      while counting_array[i] > 0
+        array[idx] = i
+        counting_array[i] -= 1
+        idx += 1
       end
     end
-    swap array, store, right
-    return store
+
+
   end
+
+
 end
---
+++ test/sort_tests/counting_sort_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+require 'counting_sort'
+
+class CountingSortTest < Minitest::Test
+
+  def setup
+    @array = [05, 03, 16, 02, 10, 14]
+    @valid_arrays_asc = [
+        { input: [1], output: [1] },
+        { input: [9, 2, 4, 5, 6, 1], output: [1, 2, 4, 5, 6, 9] },
+        { input: [0, 2, 4, 5, 6, 1], output: [0, 1, 2, 4, 5, 6] },
+    ]
+
+    @valid_arrays_desc = [
+        { input: [1], output: [1] },
+        { input: [9, 2, 4, 5, 6, 1], output: [9, 6, 5, 4, 2, 1] },
+        { input: [0, 2, 4, 5, 6, 1], output: [6, 5, 4, 2, 1, 0] }
+    ]
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test_sort_ascending
+    actual = CountingSort.order(@array, 17)
+    expected = [2, 3, 5, 10, 14, 16]
+    assert_equal expected, actual
+  end
+
+  def test_that_class_implements_sorting_interface
+    assert(CountingSort.ancestors.include?(SortingInterface), msg="Does not implement sorting interface")
+  end
+
+  def random_parameterized_test_asc
+    test_case = @valid_arrays_asc[rand(0..@valid_arrays_asc.length)]
+    actual = CountingSort.order(test_case[:input], 10)
+    assert_equal(test_case[:output], actual)
+  end
+
+  def random_parameterized_test_desc
+    test_case = @valid_arrays_desc[rand(0..@valid_arrays_desc.length)]
+    actual = CountingSort.new(test_case[:input], 10, :desc => true)
+    assert_equal(test_case[:output], actual)
+  end
+
+  def test_sort_descending
+    actual = CountingSort.order(@array, 17, :desc => true)
+    expected = [16, 14, 10, 5, 3, 2]
+    assert_equal expected, actual
+  end
+
+  def test_time_complexity_output
+    best = CountingSort::TIME_COMPLEXITY_BEST
+    worst =  CountingSort::TIME_COMPLEXITY_WORST
+    average = CountingSort::TIME_COMPLEXITY_AVERAGE
+    space = CountingSort::SPACE_COMPLEXITY
+
+    assert_equal("O(n)", worst)
+    assert_equal("O(n)", best)
+    assert_equal("O(n)", average)
+    assert_equal("O(k+n)", space)
+
+
+  end
+end
