afccb0171ceafd90db17be29609a49f4016e5cf7
uchennafokoye <okoyefaithful@gmail.com>
2016-04-21 15:08:28 -0500

    Allowed for simple graph to restrict classes allowed

--- lib/ruby_algorithms/graph/graph.rb
+++ lib/ruby_algorithms/graph/graph.rb
@@ -1,21 +1,22 @@
-#Implements a simple graph
-#See SimpleGraph
 class Graph
   attr_reader :no_of_edges, :directed
 
-  def initialize directed=false
+  def initialize options={}
     @no_of_edges = 0
     @vertices = VertexList.new
     @adjacent_vertices = Hash.new
-    @directed = directed
+    @directed = options[:directed] || false
+    allowed_classes = options[:allowed_classes] || Object
+    allowed_classes.is_a?(Array) ? parse_allowed_classes(*allowed_classes) : parse_allowed_classes(allowed_classes)
   end
 
   def add_vertex v
-    added = @vertices.add?(v)
-    if added
+    validate_vertex v
+    was_added = @vertices.add?(v)
+    if was_added
       @adjacent_vertices[v] = EdgeList.new
     end
-    return added
+    return was_added
   end
 
   def add_vertex_list *vertices
@@ -48,7 +49,9 @@ class Graph
   end
 
   def degree v
-    validate_vertex v
+    unless @vertices.include? v
+      raise "Vertex not found"
+    end
     @adjacent_vertices[v].size
   end
 
@@ -66,12 +69,34 @@ class Graph
   end
 
   private
-  attr_reader :vertices, :adjacent_vertices
-  def validate_vertex v
+  attr_reader :vertices, :adjacent_vertices, :allowed_classes
+  def parse_allowed_classes *args
+    @allowed_classes = Set.new
+    args.each do |constant|
+      validate_class_or_module constant
+      @allowed_classes.add constant
+    end
+  end
+
+  def validate_vertex_exists v
     unless @vertices.include? v
       raise "Vertex not found"
     end
   end
+
+  def validate_vertex v
+    ancestors = Set.new(v.class.ancestors)
+    unless allowed_classes.intersect? ancestors
+      raise "#{v} is not an instance of an allowed class #{allowed_classes.inspect}"
+    end
+  end
+
+  def validate_class_or_module constant
+    unless constant.is_a?(Module) || constant.is_a?(Class)
+      raise "Please provide a module or class"
+    end
+  end
+
   def is_connected v, w
     @adjacent_vertices[v].include?(w) || @adjacent_vertices[w].include?(v)
   end
@@ -82,5 +107,6 @@ class Graph
   end
 end
 
+#SimpleGraph implements a uni or bigraph with no weights
 class SimpleGraph < Graph
 end
--- test/graph_tests/simple_graph_test.rb
+++ test/graph_tests/simple_graph_test.rb
@@ -5,18 +5,50 @@ class SimpleGraphTest < Minitest::Test
 
   def setup
     @g = SimpleGraph.new
+    @graph_of_friends = SimpleGraph.new :allowed_classes=> Friend
     @f1 = Friend.new "Jane"
     @f2 = Friend.new "Hillary"
+    @s1 = Student.new "Female"
+    @s2 = Student.new "male"
   end
 
   def teardown
     # Do nothing
   end
 
+
+  def test_that_you_can_restrict_to_multiple_classes_with_array
+    graph_of_multiple_classes = SimpleGraph.new :allowed_classes => [Friend, Student]
+    assert graph_of_multiple_classes.add_vertex @f1
+    assert graph_of_multiple_classes.add_vertex @s1
+  end
+
+  def test_that_restricted_graph_allows_objects_of_allowed_classes
+    assert @graph_of_friends.add_vertex @f1
+  end
+
+  def test_that_restricted_graph_will_reject_objects_not_a_type_of_the_allowed_classes
+    begin
+      @graph_of_friends.add_vertex 1
+      fail("Expected an error to be thrown")
+    rescue
+      assert true
+    end
+  end
+
   def test_that_you_can_add_non_existing_edge_using_add_edge
     @non_existing_edge = Friend.new "Mark"
     @g.add_edge @non_existing_edge, @f1
     assert @g.vertex_list.include?(@non_existing_edge)
+  end
+
+  def test_that_you_get_true_when_you_add_vertex
+    assert @g.add_vertex @f1
+  end
+
+  def test_that_you_get_nil_or_false_when_you_add_vertex_that_already_exists
+    @g.add_vertex @f1
+    assert !(@g.add_vertex @f1)
   end
 
   def test_that_vertex_is_not_overwritten_if_exists_aka_connected_vertices_stays_the_same
@@ -46,5 +78,12 @@ class Friend
   attr_reader name
   def initialize name
     @name = name
+  end
+end
+
+class Student
+  attr_reader :gender
+  def initialize gender
+    @gender = gender
   end
 end
