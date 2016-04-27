5cfcb6797adc310dc9a5c15691bf50357fece3b0
uchennafokoye <okoyefaithful@gmail.com>
2016-04-26 19:49:27 -0500

    Basic test

--- test/data_structures/priority_queue_test.rb
+++ test/data_structures/priority_queue_test.rb
@@ -5,9 +5,24 @@ require 'priority_queue'
 class PriorityQueueTest < Minitest::Test
 
   def setup
-    @pq = PriorityQueue.new
-    @pq << 5
-    @pq << 3
+    @max_pq = PriorityQueue.new
+    @min_pq = PriorityQueue.new min_heap = true
+  end
+
+  def test_basic_priority_queue_max_heap
+    @max_pq << 6
+    @max_pq << 3
+    @max_pq << 2
+    @max_pq << 5
+    assert_equal(6, @max_pq.pop)
+  end
+
+  def test_basic_priority_queue_min_heap
+    @min_pq << 6
+    @min_pq << 3
+    @min_pq << 2
+    @min_pq << 5
+    assert_equal 2, @min_pq.pop
   end
 
 
